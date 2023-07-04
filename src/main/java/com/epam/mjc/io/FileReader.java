eed58baf4e856c6be9121dea64cc0f286cfbcf75
annKlush <109134951+annKlush@users.noreply.github.com>
2023-07-04 12:04:00 +0300

    com

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,23 +4,16 @@ import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
             StringBuilder fileContent = new StringBuilder();
-
             String line;
             while ((line = br.readLine()) != null) {
                 fileContent.append(line).append("\n");
             }
-
             String profileData = fileContent.toString();
             String[] lines = profileData.split("\n");
-            String name = "";
-            int age = 0;
-            String email = "";
-            long phone = 0;
-            Profile pr=new Profile();
+            Profile pr= new Profile();
             for (String lineData : lines) {
                 String[] keyValue = lineData.split(":");
                 String key = keyValue[0].trim();
@@ -44,11 +37,9 @@ public class FileReader {
                 }
             }
             return pr;
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return null; // Return null if an exception occurs
+        return null;
     }
 }
